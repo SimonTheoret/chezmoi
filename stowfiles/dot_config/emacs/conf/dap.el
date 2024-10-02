72b4c159bf0e7dfad546f8514203fe7748246888
Simon Théorêt <simonteoret@hotmail.com>
2024-10-01 22:11:53 -0400

    dap works for python (kind of ?)

--- stowfiles/dot_config/emacs/conf/dap.el
+++ stowfiles/dot_config/emacs/conf/dap.el
@@ -3,8 +3,12 @@
 
 ;; Debug to add bugs
 
-;; (use-package dap-mode)
+(use-package dap-mode)
 ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+(require 'dap-python)
+;; if you installed debugpy, you need to set this
+;; https://github.com/emacs-lsp/dap-mode/issues/306
+(setq dap-python-debugger 'debugpy)
 
 
  
