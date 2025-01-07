e60d4b2901d9a48247008e9f7fd20b4e79b8bb83
Simon Théorêt <simonteoret@hotmail.com>
2025-01-07 02:15:31 -0500

    welcome back emacs

---
+++ dot_config/emacs/conf/buffers.el
@@ -0,0 +1,48 @@
+;; -*- lexical-binding: t -*-
+
+
+(general-def
+  :states 'normal
+  :prefix "<leader> b"
+  :prefix-command 'Buffers
+  "i" '("Ibuffer project" . projectile-ibuffer)
+  "w" '("Ibuffer" . ibuffer)
+  "l" '("Buffer list" . consult-project-buffer)
+  "a" '("Buffer list" . consult-buffer)
+  "r" '("Rename buffer" . rename-buffer)
+  "f" '("Rename buffer and file" . crux-rename-buffer-and-file)
+  )
+
+(use-package popwin
+  :config
+  (popwin-mode 1)
+  :init
+  (setq popwin:special-display-config '(("*Miniedit Help*" :noselect t)
+					(help-mode :stick t)
+					(completion-list-mode :noselect t)
+					(compilation-mode :noselect t :stick t)
+					(grep-mode :noselect t)
+					(occur-mode :noselect t)
+					("*Pp Macroexpand Output*" :noselect t)
+					("*Async Shell Command*" :noselect t)
+					"*Shell Command Output*"
+					"*vc-diff*" "*vc-change-log*"
+					(" *undo-tree*" :width 60 :position right)
+					("^\\*anything.*\\*$" :regexp t)
+					"*slime-apropos*" "*slime-macroexpansion*" "*slime-description*"
+					("*slime-compilation*" :noselect t)
+					"*slime-xref*"
+					(sldb-mode :stick t)
+					slime-repl-mode slime-connection-list-mode)
+
+	)
+  )
+
+;; (defun maybe-quit ()
+;;   (when (string= (buffer-name) "*Async Shell Command*")
+;;     (quit-window)))
+
+;; (general-def
+;;   :states 'normal
+;;   :)
+;; (add-hook 'shell-mode-hook #'maybe-set-quit-key)
---
+++ dot_config/emacs/conf/file_types.el
@@ -0,0 +1,4 @@
+(use-package dockerfile-mode)
+(use-package cmake-mode)
+(use-package yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
