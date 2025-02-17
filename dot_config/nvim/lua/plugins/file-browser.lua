9717fcace24fd1ac9a7936d40c5db92698ed4015
Simon Théorêt <stheoret@druide.com>
2025-02-17 14:24:27 -0500

    Do not ignore hidden/gitignore in file-browser

--- dot_config/nvim/lua/plugins/file-browser.lua
+++ dot_config/nvim/lua/plugins/file-browser.lua
@@ -1,6 +1,12 @@
 return {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+        require("telescope").setup {
+            respect_gitignore = vim.fn.executable "fd" == 1,
+            no_ignore = false,
+        }
+    end,
     keys = {
 
         {
