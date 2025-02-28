f167fdf8e5a734dc91113535d51ac25c8fc39ee9
Simon Théorêt <stheoret@druide.com>
2025-02-28 13:38:30 -0500

    more python

--- dot_config/emacs/conf/pythonmode.el
+++ dot_config/emacs/conf/pythonmode.el
@@ -14,19 +14,71 @@
   :config
   (setq  python--treesit-settings
 	 (append python--treesit-settings
+		 ;; constants
 		 (treesit-font-lock-rules
 		  :feature 'custom
 		  :language 'python
-		  :override t
-		  '(((identifier) @font-lock-constant-face
-		     (:match "^_?[A-Z][A-Z_0-9]*$" @font-lock-constant-face)))
+		  :override nil
+		  '((
+		     (identifier) @font-lock-constant-face
+		     (:match "^_?[A-Z][A-Z_0-9]*$" @font-lock-constant-face)
+		     ))
 		  )
+		 ;; imports
+		 (treesit-font-lock-rules
+		  :feature 'custom
+		  :language 'python
+		  :override nil
+		  '((
+		     import_statement name: (dotted_name (identifier) @font-lock-constant-face)
+		     ))
+		  )
+		 ;; import with alias 
+		 (treesit-font-lock-rules
+		  :feature 'custom
+		  :language 'python
+		  :override nil
+		  '((
+		     import_statement
+		     name: (aliased_import
+			    name:
+			    (dotted_name (identifier) @font-lock-constant-face
+					 alias:
+					 (identifier) @font-lock-constant-face)
+			    ))
+		    )
+		  )
+		 ;; import from 
+		 (treesit-font-lock-rules
+		  :feature 'custom
+		  :language 'python
+		  :override nil
+		  '((
+		     import_from_statement module_name: (dotted_name (identifier) @font-lock-constant-face)
+		     )
+		    )
+		  )
+		 ;; import from 
+		 (treesit-font-lock-rules
+		  :feature 'custom
+		  :language 'python
+		  :override nil
+		  '((
+		     import_from_statement module_name:
+		     (relative_import
+		      (dotted_name (identifier) @font-lock-constant-face))
+		     )
+		    )
+		  )
+		 ;; Types 
 		 (treesit-font-lock-rules
 		  :feature 'custom
 		  :language 'python
 		  :override t
-		  '(((identifier) @font-lock-type-face
-		     (:match "^[A-Z].*[a-z]" @font-lock-type-face)))
+		  '((
+		     (identifier) @font-lock-type-face
+		     (:match "^[A-Z].*[a-z]" @font-lock-type-face)
+		     ))
 		  )
 		 )
 
