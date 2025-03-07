afdaac811d87205065b6bb1ae81e0a4e0b86c82e
Simon Théorêt <simonteoret@hotmail.com>
2025-03-06 22:08:09 -0500

    Removed rust queries

--- dot_config/emacs/conf/rustmode.el
+++ dot_config/emacs/conf/rustmode.el
@@ -2,35 +2,4 @@
 (use-package rust-mode
   :init
   (setq rust-mode-treesitter-derive t)
-  :config
-  (setq  rust-ts-mode--font-lock-settings
-	 (append rust-ts-mode--font-lock-settings
-		 ;; lifetime
-		 (treesit-font-lock-rules
-		  :feature 'custom
-		  :language 'rust
-		  :override nil
-		  '(
-		    (lifetime "'" @font-lock-keyword-face)
-		    )
-		  ;; lifetime
-		  :feature 'custom
-		  :language 'rust
-		  :override nil
-		  '(
-		    (lifetime
-		     (identifier) @font-lock-keyword-face)
-		    )
-		  ;; lifetime
-		  :feature 'custom
-		  :language 'rust
-		  :override nil
-		  '("(
- 		     lifetime
- 		     (identifier) @font-lock-keyword-face
-                       (#any-of? @font-lock-keyword-face "static" "_")
- 		     )")
-		  )
-		 )
-	 )
   )
