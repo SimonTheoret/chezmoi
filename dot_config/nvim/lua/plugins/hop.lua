359ef7e0168f7ca5e42f4eb3e8b47409535a8bd3
Simon Théorêt <simonteoret@hotmail.com>
2025-03-08 00:30:16 -0500

    Added new command to hop, HopLines

--- dot_config/nvim/lua/plugins/hop.lua
+++ dot_config/nvim/lua/plugins/hop.lua
@@ -2,7 +2,17 @@ return {
     'smoka7/hop.nvim',
     opts = {},
     keys = {
-     { '<leader>w', ':HopWord<CR>' ,
-    desc = "Hop to word" },
-    },
+        {
+            '<leader>w',
+            ':HopWord<CR>',
+            desc = "Hop to word",
+            mode = {"n", "v"},
+        }, 
+        {
+            '<leader>k',
+            ':HopLine<CR>',
+            desc = "Hop to line",
+            mode = {"n", "v"},
+        }, 
+        }
 }
