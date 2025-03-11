a703a058c3c8034def6ec9fb03bd324923677dd5
Simon Théorêt <stheoret@druide.com>
2025-03-11 15:23:45 -0400

    Configuration has changed!

--- dot_config/nvim/lua/plugins/nvimtree.lua
+++ dot_config/nvim/lua/plugins/nvimtree.lua
@@ -1,26 +1,13 @@
 return {
     "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     config = function()
-        -- optionally enable 24-bit colour
-        vim.opt.termguicolors = true
-        local function my_on_attach(bufnr)
-            local api = require "nvim-tree.api"
-
-            local function opts(desc)
-                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-            end
-
-            -- default mappings
-            api.config.mappings.default_on_attach(bufnr)
-
-            -- custom mappings
-            vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-        end
-
-        -- pass to setup along with your other options
         require("nvim-tree").setup {
-            view = {side = "right"},
-            on_attach = my_on_attach,
+            view = { side = "right" },
         }
     end,
     keys = {
