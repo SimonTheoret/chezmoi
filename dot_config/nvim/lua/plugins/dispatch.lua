08cc9c8020af2323797c80d3bad1d76770a902a8
Simon Théorêt <stheoret@druide.com>
2025-03-12 09:29:26 -0400

    Added recompile capacity

--- dot_config/nvim/lua/plugins/dispatch.lua
+++ dot_config/nvim/lua/plugins/dispatch.lua
@@ -1,9 +1,10 @@
 return {
     "tpope/vim-dispatch",
     keys = {
-        { "<leader>cm", ":Make! ", desc = "Compile" },
+        { "<leader>cm", ":Make! ", desc = "Make" },
         { "<leader>cq", ":Copen<CR>", desc = "Compilation quicklist" },
-        { "<leader>cc", ":Dispatch ", desc = "Dispatch" },
+        { "<leader>cc", ":Dispatch ", desc = "Compile" },
+        { "<leader>cr", ":Copen|Dispatch<CR>", desc = "Recompile" },
         { "<leader>ci", ":Start ", desc = "Interactive process" },
         { "<leader>cn", ":cn<CR>", desc = "Next error" },
         { "<leader>cp", ":cp<CR>", desc = "Previous error" },
