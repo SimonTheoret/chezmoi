bf768f22e6d1168730559260f47536c1d51618fc
Simon Théorêt <simonteoret@hotmail.com>
2025-04-04 23:40:37 -0400

    docs: added documentation for git_signs.lua

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -19,7 +19,7 @@ return {
                     else
                         gitsigns.nav_hunk('next')
                     end
-                end)
+                end, { desc = "Next hunk" })
 
                 map('n', '[c', function()
                     if vim.wo.diff then
@@ -27,45 +27,73 @@ return {
                     else
                         gitsigns.nav_hunk('prev')
                     end
-                end)
+                end, { desc = "Previous hunk" })
 
                 -- Actions
-                map('n', '<leader>hs', gitsigns.stage_hunk)
-                map('n', '<leader>hr', gitsigns.reset_hunk)
+                map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "Stage hunk" })
+                map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "Reset hunk" })
 
                 map('v', '<leader>hs', function()
                     gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+                    { desc = "Stage hunk" }
                 end)
 
                 map('v', '<leader>hr', function()
                     gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+                    { desc = "Reset hunk" }
                 end)
 
-                map('n', '<leader>hS', gitsigns.stage_buffer)
-                map('n', '<leader>hR', gitsigns.reset_buffer)
-                map('n', '<leader>hp', gitsigns.preview_hunk)
-                map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+                map('n', '<leader>hS', gitsigns.stage_buffer,
+
+                    { desc = "Stage buffer" }
+                )
+                map('n', '<leader>hR', gitsigns.reset_buffer,
+                    { desc = "Reset buffer" }
+                )
+                map('n', '<leader>hp', gitsigns.preview_hunk,
+                    { desc = "Preview hunk" }
+                )
+                map('n', '<leader>hi', gitsigns.preview_hunk_inline,
+                    { desc = "Preview hunk inline" }
+                )
 
                 map('n', '<leader>hb', function()
-                    gitsigns.blame_line({ full = true })
-                end)
+                        gitsigns.blame_line({ full = true })
+                    end,
 
-                map('n', '<leader>hd', gitsigns.diffthis)
+                    { desc = "Blame line" }
+                )
+
+                map('n', '<leader>hd', gitsigns.diffthis,
+                    { desc = "Diff this" }
+                )
 
                 map('n', '<leader>hD', function()
-                    gitsigns.diffthis('~')
-                end)
+                        gitsigns.diffthis('~')
+                    end,
 
-                map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
-                map('n', '<leader>hq', gitsigns.setqflist)
+                    { desc = "Diff this ~" }
+                )
+
+                map('n', '<leader>hQ', function() gitsigns.setqflist('all') end,
+
+                    { desc = "All hunks to qflist" }
+                )
+                map('n', '<leader>hq', gitsigns.setqflist,
+                    { desc = "Buffer hunks to qflist" }
+                )
 
                 -- Toggles
-                map('n', '<leader>htb', gitsigns.toggle_current_line_blame)
-                map('n', '<leader>htd', gitsigns.toggle_deleted)
-                map('n', '<leader>htw', gitsigns.toggle_word_diff)
+                map('n', '<leader>htb', gitsigns.toggle_current_line_blame,
+                    { desc = "Toggle current line blame" }
+                )
+                map('n', '<leader>htw', gitsigns.toggle_word_diff,
+                    { desc = "Toggle word diff" }
+                )
 
                 -- Text object
-                map({ 'o', 'x' }, 'ih', gitsigns.select_hunk)
+                map({ 'o', 'x' }, 'ih', gitsigns.select_hunk,
+                    { desc = "Select hunk" })
             end
         }
     end
