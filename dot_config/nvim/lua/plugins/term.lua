e3b8bf2d9c1521ac620d210df733e3ce32cdbabc
Simon Théorêt <simonteoret@hotmail.com>
2025-05-19 11:25:09 -0400

    feat(nvim): configured toggle term

---
+++ dot_config/nvim/lua/plugins/term.lua
@@ -0,0 +1,17 @@
+return {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+        require("toggleterm").setup {
+            -- size can be a number or function which is passed the current terminal
+            function(term)
+                if term.direction == "horizontal" then
+                    return 15
+                elseif term.direction == "vertical" then
+                    return vim.o.columns * 0.4
+                end
+            end,
+            open_mapping = [[<leader>tt]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+        }
+    end,
+}
