9fa74f2d51d8d9044bd533163a33b90fdba0bcb6
Simon Théorêt <simonteoret@hotmail.com>
2025-05-23 22:45:26 -0400

    fix: avoid calling git fsmonitor--daemon status

--- bin/executable_git_faster.sh
+++ bin/executable_git_faster.sh
@@ -4,17 +4,11 @@ git config feature.manyFiles true
 
 git update-index --index-version 4
 
-git config core.fsmonitor true
-
-git fsmonitor--daemon status
-
 git config core.commitgraph true
-
 
 if [ "$(uname -s)" = "Linux" ]; then
   echo "Using rs-git-fsmonitor"
   git config core.fsmonitor rs-git-fsmonitor
-  git fsmonitor--daemon status
 else
   echo "This is not Linux."
   git config core.fsmonitor true
