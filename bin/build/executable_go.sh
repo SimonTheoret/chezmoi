ec8e3d1dc7e7b4814a8b7c524bcef9bda98fa7c3
Simon Théorêt <simonteoret@hotmail.com>
2025-05-24 19:06:46 -0400

    feat: configured go setup, added alacritty conf for popos

--- bin/build/executable_go.sh
+++ bin/build/executable_go.sh
@@ -1,8 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 echo "Setup for Golang"
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
+os_name=$(uname -s)
+
+arch=$(uname -i)
+if [ $arch="x84_64" ]; then
+    arch="amd64"
+fi
+version="1.24.3"
+file="go${version}.${os_name,,}-${arch,,}.tar.gz"
+destination="https://go.dev/dl/${file}"
+echo "Download url: ${destination}"
+wget "${destination}"
+
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${file}
 
 command -v go
