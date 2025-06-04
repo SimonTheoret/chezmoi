b99d6f6fe08d760418cca633f434967e93490b3f
Simon Théorêt <stheoret@druide.com>
2025-06-04 14:03:51 +0000

    WIP

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,4 +3,14 @@ return {
     lazy = false,
     branch = 'main',
     build = ':TSUpdate',
+    config = function()
+        require 'nvim-treesitter'.install {
+            "lua", "vim", "vimdoc",
+            "regex", "markdown", "markdown_inline",
+            "bash", "latex", "bibtex",
+            "css", "html", "javascript",
+            "python", "rust", "go",
+            "gleam", "elixir", "xml"
+        }
+    end
 }
