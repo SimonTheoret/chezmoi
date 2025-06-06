02f6621c686c3fa209cfe39b33c902fe7c746709
Simon Théorêt <simonteoret@hotmail.com>
2025-06-05 23:16:07 -0400

    fix(nvim): files are correctly HL with TS

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,13 +4,21 @@ return {
     branch = 'main',
     build = ':TSUpdate',
     config = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("EnableTreesitterHighlighting", { clear = true }),
+            desc = "Try to enable tree-sitter syntax highlighting",
+            pattern = "*", -- run on *all* filetypes
+            callback = function()
+                pcall(function() vim.treesitter.start() end)
+            end,
+        })
         require 'nvim-treesitter'.install {
             "lua", "vim", "vimdoc",
             "regex", "markdown", "markdown_inline",
             "bash", "latex", "bibtex",
             "css", "html", "javascript",
             "python", "rust", "go",
-            "gleam", "elixir", "xml"
+            "gleam", "elixir", "xml", "zig"
         }
     end
 }
