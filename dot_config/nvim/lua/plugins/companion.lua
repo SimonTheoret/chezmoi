f7b3364ac967d375bba1be202b2decba33f47231
Simon Théorêt <simonteoret@hotmail.com>
2025-06-07 17:31:24 -0400

    feat(nvim): code companion is operational

--- dot_config/nvim/lua/plugins/companion.lua
+++ dot_config/nvim/lua/plugins/companion.lua
@@ -1,9 +1,27 @@
 return {
-    lazy = true,
     "olimorris/codecompanion.nvim",
-    opts = {},
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
+    config = function()
+        require('codecompanion').setup({
+            strategies = {
+                chat = { adapter = "ollama" },
+                inline = { adapter = "ollama" },
+                cmd = { adapter = "ollama" }
+            },
+            opts = {
+                send_code = false,
+            },
+            display = {
+                action_palette = {
+                    provider = "fzf_lua", -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+                }
+            },
+        })
+    end,
+    keys = {
+        { "<leader>llm", ":CodeCompanionActions<CR>", desc = "Code companion" }
+    }
 }
