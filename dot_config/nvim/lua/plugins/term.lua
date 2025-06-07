64d2a675e35d9b82808b6b384b277006d6d5f727
Simon Théorêt <simonteoret@hotmail.com>
2025-06-07 13:09:24 -0400

    feat(nvim): can toggle vertically term

--- dot_config/nvim/lua/plugins/term.lua
+++ dot_config/nvim/lua/plugins/term.lua
@@ -4,17 +4,30 @@ return {
     config = function()
         require("toggleterm").setup {
             -- size can be a number or function which is passed the current terminal
-            size  = function(term)
+            size              = function(term)
                 if term.direction == "horizontal" then
                     return 15
                 elseif term.direction == "vertical" then
                     return vim.o.columns * 0.4
                 end
             end,
-            open_mapping = [[<leader>tt]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+            -- open_mapping = [[<leader>tt]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
             terminal_mappings = true,
-            insert_mappings = false,
+            insert_mappings   = false,
         }
     end,
-    event = "VeryLazy"
+    event = "VeryLazy",
+    keys = {
+        {
+
+            "<leader>tt",
+            ":ToggleTerm<CR>",
+            desc = "Toggle term"
+        },
+        {
+            "<leader>tn",
+            ":ToggleTerm direction=vertical<CR>",
+            desc = "Toggle term"
+        }
+    }
 }
