910c403c41f4700a989ac6e6a2f8a27331639a30
Simon Théorêt <simonteoret@hotmail.com>
2025-06-23 21:01:53 -0400

    fix: fixed commit litns

--- bin/executable_setup_commitlint.sh
+++ bin/executable_setup_commitlint.sh
@@ -4,14 +4,14 @@ set -e
 
 npm install --save-dev @commitlint/{cli,config-conventional}
 
-echo "export default { extends: ['@commitlint/config-conventional'] };" > commitlint.config.js
+echo "export default { extends: ['@commitlint/config-conventional'] };" >commitlint.config.mjs
 
 npm install --save-dev husky
 
 npx husky init
 
 # Add commit message linting to commit-msg hook
-echo "npx --no -- commitlint --edit \$1" > .husky/commit-msg
+echo "npx --no -- commitlint --edit \$1" >.husky/commit-msg
 
 # Windows users should use ` to escape dollar signs
 # echo "npx --no commitlint --edit `$1" > .husky/commit-msg
