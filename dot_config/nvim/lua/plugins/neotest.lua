53e29fe2b5772ce050e8006c39fce24184fd8233
Simon Théorêt <stheoret@druide.com>
2025-07-10 18:39:51 +0000

    fix(nvim): avoid auto closing the neotest terminal output

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -46,8 +46,8 @@ return {
         },
         { "<leader>tl", function() require("neotest").run.run_last() end,                                           desc = "Run last" },
         { "<leader>ts", function() require("neotest").summary.toggle() end,                                         desc = "Toggle summary" },
-        { "<leader>tO", function() require("neotest").output_panel.toggle({ enter = true, auto_close = true }) end, desc = "Show output" },
-        { "<leader>to", function() require("neotest").output_panel.toggle() end,                                    desc = "Toggle output panel" },
+        { "<leader>to", function() require("neotest").output_panel.toggle({ enter = true, auto_close = false }) end, desc = "Show output" },
+        { "<leader>tO", function() require("neotest").output_panel.toggle() end,                                    desc = "Toggle output panel" },
         { "<leader>tS", function() require("neotest").run.stop() end,                                               desc = "Stop test" },
         { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end,                         desc = "Toggle watch" },
     }
