4aa4d2618745858dbb449bb82ab083e216a06e4a
Simon Théorêt <simonteoret@hotmail.com>
2025-07-24 00:06:08 -0400

    feat(nvim): added multiple configurations for  ollama adapters

--- dot_config/nvim/lua/plugins/companion.lua
+++ dot_config/nvim/lua/plugins/companion.lua
@@ -19,6 +19,25 @@ return {
                     provider = "fzf_lua", -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
                 }
             },
+            adapters = {
+                Dev = function()
+                    return require("codecompanion.adapters").extend("ollama", {
+                        name = "Dev",
+                        schema = {
+                            model = {
+                                order = 1,
+                                type = "enum",
+                                default = "qwen2.5-coder:7b",
+                                choices = {
+                                    "qwen2.5-coder:7b",
+                                    "qwen2.5-coder:32b",
+                                    "devstral:24b",
+                                }
+                            },
+                        },
+                    })
+                end,
+            }
         })
     end,
     keys = {
