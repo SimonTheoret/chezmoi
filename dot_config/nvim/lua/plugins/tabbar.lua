fa60dc69d28f7ba55cb23aaea2aa4661ac1d6197
Simon Théorêt <simonteoret@hotmail.com>
2025-07-26 14:01:44 -0400

    feat(nvim): used faster tabline

--- dot_config/nvim/lua/plugins/tabbar.lua
+++ dot_config/nvim/lua/plugins/tabbar.lua
@@ -1,19 +1,43 @@
+-- return {
+--     'romgrk/barbar.nvim',
+--     dependencies = {
+--         'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+--         'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+--     },
+--     init = function() vim.g.barbar_auto_setup = false end,
+--     opts = {
+--         -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+--         animation = false,
+--         -- insert_at_start = true,
+--         -- …etc.
+--     },
+--     event = "VeryLazy",
+--     keys = {
+--         {"L", ":BufferNext<CR>", "Next tab"},
+--         {"H", ":BufferPrevious<CR>", "Previous tab"}
+--     }
+-- }
 return {
-    'romgrk/barbar.nvim',
-    dependencies = {
-        'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-        'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-        -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-        animation = false,
-        -- insert_at_start = true,
-        -- …etc.
-    },
-    event = "VeryLazy",
+    'echasnovski/mini.tabline',
+    version = '*',
+    config = true,
+    lazy= false,
     keys = {
-        {"L", ":BufferNext<CR>", "Next tab"},
-        {"H", ":BufferPrevious<CR>", "Previous tab"}
-    }
+        { "L", ":bnext<CR>",     "Next tab" },
+        { "H", ":bprevious<CR>", "Previous tab" }
+    },
 }
+-- return {
+--     'nanozuki/tabby.nvim',
+--     ---@type TabbyConfig
+--     opts = {
+--         preset = 'active_tab_with_wins',
+--     },
+--     keys = {
+--         { "L",     ":tabn<CR>",     "Next tab" },
+--         { "H",     ":tabp<CR>",     "Previous tab" },
+--         { "<C-l>", ":$tabnew<CR>",  "Next tab" },
+--         { "<C-h>", ":tabclose<CR>", "Previous tab" },
+--     },
+--     lazy = "VeryLazy",
+-- }
