48fe284d65a6a9218c6cb273b1e73bd965cfe45f
Simon Théorêt <simonteoret@hotmail.com>
2025-07-27 15:35:17 -0400

    feat(nvim): added tardis (git time machine)

---
+++ dot_config/nvim/lua/plugins/tardis.lua
@@ -0,0 +1,24 @@
+return
+{
+    'fredeeb/tardis.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require('tardis-nvim').setup {
+            keymap = {
+                ["next"] = '<C-j>',     -- next entry in log (older)
+                ["prev"] = '<C-k>',     -- previous entry in log (newer)
+                ["quit"] = 'q',         -- quit all
+                ["revision_message"] = '<C-m>', -- show revision message for current revision
+                ["commit"] = '<C-g>',   -- replace contents of origin buffer with contents of tardis buffer
+            },
+            settings = {
+                initial_revisions = 10, -- initial revisions to create buffers for
+                max_revisions = 256, -- max number of revisions to load
+                show_commit_index = true, -- append [index|total] to buffer names when browsing revisions
+            },
+        }
+    end,
+    keys = {
+        { "<leader>hg", ":Tardis git<CR>", desc = "Tardis" },
+    }
+}
