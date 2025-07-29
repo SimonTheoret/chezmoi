3f1d1cf7be6cee9e7bfa2945531bdd90ca6bc092
Simon Théorêt <simonteoret@hotmail.com>
2025-07-28 22:36:41 -0400

    feat(nvim): moved from trouble.nvim to quicker.nvim

--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -1,56 +1,56 @@
 return {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = "Trouble",
-    keys = {
-        {
-            "<leader>xX",
-            ":Trouble diagnostics toggle focus=true<CR>",
-            desc = "Diagnostics (Trouble)",
-        },
-        {
-            "<leader>xx",
-            ":Trouble diagnostics toggle filter.buf=0 focus=true<CR>",
-            desc = "Buffer Diagnostics (Trouble)",
-        },
-        {
-            "<leader>xl",
-            ":Trouble loclist toggle focus=true<CR>",
-            desc = "Location List (Trouble)",
-        },
-        {
-            "<leader>xq",
-            ":Trouble qflist toggle focus=true<CR>",
-            desc = "Quickfix List (Trouble)",
-        },
-        {
-            "<leader>xa",
-            ":Trouble <CR>",
-            desc = "All (Trouble)",
-        },
-        -- {
-        --     "<leader>xp",
-        --     function()
-        --         require('trouble').previous()
-        --     end,
-        --     desc = "Previous (Trouble)",
-        -- },
-        -- {
-        --     "<leader>xn",
-        --     function()
-        --         require('trouble').next()
-        --     end,
-        --     desc = "Next (Trouble)",
-        -- },
-        {
-            "<leader>lt",
-            ":Trouble symbols toggle focus=false<cr>",
-            desc = "Symbols (Trouble)",
-        },
-        {
-            "<leader>lc",
-            ":Trouble lsp toggle focus=false <cr>",
-            desc = "LSP Definitions / references / ... (Trouble)",
-        },
-    },
+    -- "folke/trouble.nvim",
+    -- opts = {}, -- for default options, refer to the configuration section for custom setup.
+    -- cmd = "Trouble",
+    -- keys = {
+    --     {
+    --         "<leader>xX",
+    --         ":Trouble diagnostics toggle focus=true<CR>",
+    --         desc = "Diagnostics (Trouble)",
+    --     },
+    --     {
+    --         "<leader>xx",
+    --         ":Trouble diagnostics toggle filter.buf=0 focus=true<CR>",
+    --         desc = "Buffer Diagnostics (Trouble)",
+    --     },
+    --     {
+    --         "<leader>xl",
+    --         ":Trouble loclist toggle focus=true<CR>",
+    --         desc = "Location List (Trouble)",
+    --     },
+    --     {
+    --         "<leader>xq",
+    --         ":Trouble qflist toggle focus=true<CR>",
+    --         desc = "Quickfix List (Trouble)",
+    --     },
+    --     {
+    --         "<leader>xa",
+    --         ":Trouble <CR>",
+    --         desc = "All (Trouble)",
+    --     },
+    --     -- {
+    --     --     "<leader>xp",
+    --     --     function()
+    --     --         require('trouble').previous()
+    --     --     end,
+    --     --     desc = "Previous (Trouble)",
+    --     -- },
+    --     -- {
+    --     --     "<leader>xn",
+    --     --     function()
+    --     --         require('trouble').next()
+    --     --     end,
+    --     --     desc = "Next (Trouble)",
+    --     -- },
+    --     {
+    --         "<leader>lt",
+    --         ":Trouble symbols toggle focus=false<cr>",
+    --         desc = "Symbols (Trouble)",
+    --     },
+    --     {
+    --         "<leader>lc",
+    --         ":Trouble lsp toggle focus=false <cr>",
+    --         desc = "LSP Definitions / references / ... (Trouble)",
+    --     },
+    -- },
 }
