ce24ebfcf51d81a2616b3a8f6758aedbc1b379f1
Simon Théorêt <simonteoret@hotmail.com>
2025-08-03 13:12:36 -0400

    feat(sync): added resync to restart syncing

--- bin/executable_sync.sh
+++ bin/executable_sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-rclone bisync org:org-notes ~/org --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --fix-case --resync-mode newer
+rclone bisync org:org-notes ~/org --create-empty-src-dirs --compare size,modtime,checksum --slow-hash-sync-only -MvP --fix-case --resync-mode newer --conflict-resolve newer
