03f760437352890d92ea3ceef5e4be2ee9d73844
Simon Théorêt <stheoret@druide.com>
2025-09-12 10:39:20 -0400

    feat(nvim): nvim-surround -> mini.surround

--- dot_config/nvim/lua/plugins/surround.lua
+++ dot_config/nvim/lua/plugins/surround.lua
@@ -1,10 +1,29 @@
+-- return {
+--     "kylechui/nvim-surround",
+--     event = "VeryLazy",
+--     config = function()
+-- ---@diagnostic disable-next-line: missing-fields
+--         require("nvim-surround").setup({
+--             -- Configuration here, or leave empty to use defaults
+--         })
+--     end,
+-- }
 return {
-    "kylechui/nvim-surround",
-    event = "VeryLazy",
-    config = function()
----@diagnostic disable-next-line: missing-fields
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end,
+    {
+        'nvim-mini/mini.surround',
+        version = '*',
+        config = function()
+            require('mini.surround').setup({
+                -- mappings = {
+                --     add = 'ma',       -- Add surrounding in Normal and Visual modes
+                --     delete = 'md',    -- Delete surrounding
+                --     find = 'mf',      -- Find surrounding (to the right)
+                --     find_left = 'mF', -- Find surrounding (to the left)
+                --     highlight = 'mh', -- Highlight surrounding
+                --     replace = 'mr',   -- Replace surrounding
+                --
+                -- },
+            })
+        end
+    }
 }
