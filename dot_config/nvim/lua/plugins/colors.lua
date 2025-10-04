5030e658a13672b23a19173ed5267893378f234f
Simon Théorêt <simonteoret@hotmail.com>
2025-10-04 11:12:26 -0400

    feat(nvim): set background color for terminal and lazygit

--- dot_config/nvim/lua/plugins/colors.lua
+++ dot_config/nvim/lua/plugins/colors.lua
@@ -234,6 +234,16 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
+        -- Modified the background for the
+        local function set_normal_float_highlight()
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1f1e1e" })
+        end
+
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "*",
+            callback = set_normal_float_highlight,
+        })
+
         require("gruvbox").setup({
         })
         vim.o.background = "dark" -- or "light" for light mode
