e8f74277d0ece51cf4402a5d5a4c65941535cad5
Simon Théorêt <stheoret@druide.com>
2025-10-08 14:30:05 -0400

    fix(nvim): fix conflicting keymap

--- dot_config/nvim/lua/simon/keymaps.lua
+++ dot_config/nvim/lua/simon/keymaps.lua
@@ -51,7 +51,7 @@ vmap.set("n", "<leader>bb", ":only<CR>", { desc = "Focus buffer" })
 
 function _G.set_terminal_keymaps()
     local opts = { buffer = 0 }
-    vim.keymap.set('t', '<C-n><C-n>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-m><C-m>', [[<C-\><C-n>]], opts)
     vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
     vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
     vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
