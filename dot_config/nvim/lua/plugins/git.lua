015a4f98b4dc8e27b39717b41538947c5b866007
Simon Théorêt <simonteoret@hotmail.com>
2025-10-16 20:58:48 -0400

    feat(nvim): configured neogit, new kb for lazygit

---
+++ dot_config/nvim/lua/plugins/git.lua
@@ -0,0 +1,22 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",  -- required
+        "sindrets/diffview.nvim", -- optional - Diff integration
+        "folke/snacks.nvim",      -- optional
+    },
+    keys = {
+        {
+            "<leader>gg",
+            function() require('neogit').open() end,
+            desc = "Lazygit"
+        },
+        {
+            "<leader>g?",
+            ":Neogit help<CR>",
+            desc = "Neogit help"
+        },
+
+    }
+
+}
