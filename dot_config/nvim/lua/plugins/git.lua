fa4e36922c3ef2064722cc796a7cfee3a0312319
Simon Théorêt <simonteoret@hotmail.com>
2025-10-18 21:46:52 -0400

    feat(nvim): configured split for neogit

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -5,6 +5,22 @@ return {
         "sindrets/diffview.nvim", -- optional - Diff integration
         "folke/snacks.nvim",      -- optional
     },
+    config = function()
+        require('neogit').setup({
+            graph_style = "unicode",
+            commit_editor = {
+                kind = "tab",
+                -- Accepted values:
+                -- "split" to show the staged diff below the commit editor
+                -- "vsplit" to show it to the right
+                -- "split_above" Like :top split
+                -- "vsplit_left" like :vsplit, but open to the left
+                -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
+                staged_diff_split_kind = "auto",
+                spell_check = true,
+            },
+        })
+    end,
     keys = {
         {
             "<leader>gg",
