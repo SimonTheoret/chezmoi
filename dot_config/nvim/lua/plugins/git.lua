7ab5afb990c270809d9c62893913ea39c81c01a7
Simon Théorêt <simonteoret@hotmail.com>
2025-10-29 21:20:12 -0400

    feat(nvim): sticking to neogit

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,54 +1,63 @@
 return {
-    {
-        "tpope/vim-fugitive",
-        lazy = false,
-        keys = { { "<leader>gg", ":G<CR>", desc = "Fugitive" } }
-    },
-    {
-        "rbong/vim-flog",
-        lazy = true,
-        cmd = { "Flog", "Flogsplit", "Floggit" },
-        dependencies = {
-            "tpope/vim-fugitive",
-        },
-        keys = {
-            { "<leader>gl", ":Flogsplit<CR>", desc = "Flog" },
-            -- { "<leader>gls", ":Flog<CR>",      desc = "Flog" },
-        }
-    },
     -- {
-    --     "NeogitOrg/neogit",
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim",  -- required                                     }
-    --         "sindrets/diffview.nvim", -- optional - Diff integration
-    --         "folke/snacks.nvim",      -- optional
-    --     },
+    --     'nvim-mini/mini-git',
+    --     version = false,
+    --     lazy = false,
     --     config = function()
-    --         require('neogit').setup({
-    --             commit_editor = {
-    --                 kind = "tab",
-    --                 -- Accepted values:
-    --                 -- "split" to show the staged diff below the commit editor
-    --                 -- "vsplit" to show it to the right
-    --                 -- "split_above" Like :top split
-    --                 -- "vsplit_left" like :vsplit, but open to the left
-    --                 -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
-    --                 staged_diff_split_kind = "auto",
-    --                 spell_check = true,
-    --             },
-    --         })
+    --         require("mini.git").setup()
     --     end,
+    --     keys = { { "<leader>gs", ":Git", desc = "Git" } }
+    -- },
+    -- {
+    --     "tpope/vim-fugitive",
+    --     lazy = false,
+    --     keys = { { "<leader>gg", ":G<CR>", desc = "Fugitive" } }
+    -- },
+    -- {
+    --     "rbong/vim-flog",
+    --     lazy = true,
+    --     cmd = { "Flog", "Flogsplit", "Floggit" },
+    --     dependencies = {
+    --         "tpope/vim-fugitive",
+    --     },
     --     keys = {
-    --         {
-    --             "<leader>gg",
-    --             function() require('neogit').open() end,
-    --             desc = "Lazygit"
-    --         },
-    --         {
-    --             "<leader>gi",
-    --             ":Neogit help<CR>",
-    --             desc = "Neogit help"
-    --         },
+    --         { "<leader>gl", ":Flogsplit<CR>", desc = "Flog" },
+    --         -- { "<leader>gls", ":Flog<CR>",      desc = "Flog" },
     --     }
-    -- }
+    -- },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",  -- required                                     }
+            "sindrets/diffview.nvim", -- optional - Diff integration
+            "folke/snacks.nvim",      -- optional
+        },
+        config = function()
+            require('neogit').setup({
+                commit_editor = {
+                    kind = "tab",
+                    -- Accepted values:
+                    -- "split" to show the staged diff below the commit editor
+                    -- "vsplit" to show it to the right
+                    -- "split_above" Like :top split
+                    -- "vsplit_left" like :vsplit, but open to the left
+                    -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
+                    staged_diff_split_kind = "auto",
+                    spell_check = true,
+                },
+            })
+        end,
+        keys = {
+            {
+                "<leader>gg",
+                function() require('neogit').open() end,
+                desc = "Lazygit"
+            },
+            {
+                "<leader>gi",
+                ":Neogit help<CR>",
+                desc = "Neogit help"
+            },
+        }
+    }
 }
