24b8ad14df254cb8d32df473ec5759f5df4cea83
Simon Théorêt <simonteoret@hotmail.com>
2025-10-29 21:51:16 -0400

    fix(nvim): removing unused go from none-ls

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -9,12 +9,7 @@ return {
                 -- generic
                 null_ls.builtins.diagnostics.trail_space,
                 -- go
-                null_ls.builtins.formatting.golines,
-                null_ls.builtins.formatting.goimports_reviser,
-                null_ls.builtins.formatting.gofumpt,        -- stricter than gofmt
-                null_ls.builtins.diagnostics.golangci_lint,
-                null_ls.builtins.code_actions.gomodifytags, -- modify tags easily
-                null_ls.builtins.code_actions.impl,         -- generate method stubs for interfaces implementation
+                null_ls.builtins.code_actions.impl, -- generate method stubs for interfaces implementation
                 -- python
                 null_ls.builtins.formatting.isort.with({
                     extra_args = {
