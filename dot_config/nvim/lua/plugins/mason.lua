ba09772129b221832b964063fa577c634c8ee6ba
Simon Théorêt <stheoret@druide.com>
2026-01-13 16:25:36 -0500

    feat(nvim): automatic install of lua ls

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,14 +1,11 @@
 return {
     "williamboman/mason.nvim",
     config = function()
-        local lib = require("slib")
-        local ensure_installed_lsps = lib.is_executable_available("lua-language-server") and {} or
-        { "lua-language-server" }
-        require("mason").setup(
-            {
-                ensure_installed = ensure_installed_lsps
-            }
-        )
+        local slib = require('slib')
+        require("mason").setup({})
+        if not require('mason-registry').is_installed("lua-language-server") and not slib.is_executable_available("lua-language-server") then
+            vim.cmd('MasonInstall stylua lua-language-server')
+        end
     end,
     event = "VeryLazy"
 }
