c4a64b9db8d3374bcd98e3f4b7794402b9b62775
Simon Théorêt <stheoret@druide.com>
2026-01-15 16:45:14 -0500

    feat(nvim): used minidiff

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -37,57 +37,18 @@ return {
                 desc = "Git fold"
             },
         },
-    }
-    -- {
-    --     "tpope/vim-fugitive",
-    --     lazy = false,
-    --     keys = { { "<leader>gs", ":vertical G<CR>", desc = "Fugitive" }, { "<leader>gs", ":Git ", desc = "Fugitive" } }
-    -- },
-    -- {
-    --     "rbong/vim-flog",
-    --     lazy = true,
-    --     cmd = { "Flog", "Flogsplit", "Floggit" },
-    --     dependencies = {
-    --         "tpope/vim-fugitive",
-    --     },
-    --     keys = {
-    --         { "<leader>gl", ":vertical Flogsplit<CR> -max-count=1000", desc = "Flog" },
-    --         -- { "<leader>gls", ":Flog<CR>",      desc = "Flog" },
-    --     }
-    -- },
-    -- {
-    --     "NeogitOrg/neogit",
-    --     dependencies = {
-    --         "nvim-lua/plenary.nvim",  -- required                                     }
-    --         "sindrets/diffview.nvim", -- optional - Diff integration
-    --         "folke/snacks.nvim",      -- optional
-    --     },
-    --     config = function()
-    --         require('neogit').setup({
-    --             commit_editor = {
-    --                 kind = "tab",
-    --                 -- Accepted values:
-    --                 -- "split" to show the staged diff below the commit editor
-    --                 -- "vsplit" to show it to the right
-    --                 -- "split_above" Like :top split
-    --                 -- "vsplit_left" like :vsplit, but open to the left
-    --                 -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
-    --                 staged_diff_split_kind = "auto",
-    --                 spell_check = true,
-    --             },
-    --         })
-    --     end,
-    --     keys = {
-    --         {
-    --             "<leader>gg",
-    --             function() require('neogit').open() end,
-    --             desc = "Lazygit"
-    --         },
-    --         {
-    --             "<leader>gi",
-    --             ":Neogit help<CR>",
-    --             desc = "Neogit help"
-    --         },
-    --     }
-    -- }
+    },
+    {
+        'nvim-mini/mini.diff',
+        event = "VeryLazy",
+        version = '*',
+        config = true,
+        keys = {
+            {
+                "<leader>gt", function()
+                MiniDiff.toggle_overlay()
+            end
+            }
+        }
+    },
 }
