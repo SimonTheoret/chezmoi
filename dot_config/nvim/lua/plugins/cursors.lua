1ceaf1b8b384c0900fb12adb3980c021b474f4ad
Simon Théorêt <stheoret@druide.com>
2026-05-20 11:17:32 -0400

    feat(nvim): cleanup!

--- dot_config/nvim/lua/plugins/cursors.lua
+++ dot_config/nvim/lua/plugins/cursors.lua
@@ -19,11 +19,6 @@ return {
         set({ "n", "x" }, "<leader>N", function() mc.matchAddCursor(-1) end)
         set({ "n", "x" }, "<leader>S", function() mc.matchSkipCursor(-1) end)
 
-        -- Add and remove cursors with control + left click.
-        set("n", "<c-leftmouse>", mc.handleMouse)
-        set("n", "<c-leftdrag>", mc.handleMouseDrag)
-        set("n", "<c-leftrelease>", mc.handleMouseRelease)
-
         -- Disable and enable cursors.
         set({ "n", "x" }, "<c-q>", mc.toggleCursor)
 
--- dot_config/nvim/lua/simon/keymaps.lua
+++ dot_config/nvim/lua/simon/keymaps.lua
@@ -1,14 +1,5 @@
 -- Useful keymaps:
 local vmap = vim.keymap
-local lib = require('slib')
-
--- vmap.set("n", "<leader>q", function()
---     lib.toggle_qf()
--- end, { desc = "Toggle quickfix" })
--- --
--- vmap.set("n", "<leader>ll", function()
---     lib.toggle_loclist()
--- end, { desc = "Toggle loclist" })
 
 -- Move selection up or down
 vmap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -25,6 +16,7 @@ vmap.set("n", "N", "Nzzzv")
 -- Copy the clipboard content into system clipboard
 vmap.set({ "n", "v" }, "<leader>y", [["+y]],
     { desc = "Copy clipboard into system clipboard" })
+
 vmap.set("n", "<leader>Y", [["+Y]],
     { desc = "Copy clipboard into system clipboard with overwrite" })
 
@@ -50,7 +42,7 @@ vmap.set("n", "<leader>cd", ":cd %:h<CR>", { desc = "Make file's path as working
 vmap.set("n", "<leader>pwd", ":lua print(vim.api.nvim_buf_get_name(0))<CR>", { desc = "Print current buffer path" })
 
 -- Notes
-vmap.set("n", "<leader>nn", ":80vs notes_perso_stheoret.md<CR>", { desc = "Open notes in directory root" })
+vmap.set("n", "<leader>Nn", ":80vs notes_perso_stheoret.md<CR>", { desc = "Open notes in directory root" })
 
 -- Marks
 vmap.set("n", "<leader>ba", ":delm! | delm A-Z0-9 <CR>", { desc = "Remove all marks" })
