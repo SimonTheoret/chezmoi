2578308c494c37704e0bfb4236cc2c7cb6a0fbb4
Simon Théorêt <stheoret@druide.com>
2026-05-21 11:37:21 -0400

    feat(nvim): add mini statusline

--- dot_config/nvim/lua/plugins/colors.lua
+++ dot_config/nvim/lua/plugins/colors.lua
@@ -1,21 +1,24 @@
 return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-        -- Modified the background for the terminal (?)
-        local function set_normal_float_highlight()
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1f1e1e" })
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            -- Modified the background for the terminal (?)
+            local function set_normal_float_highlight()
+                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1f1e1e" })
+            end
+
+            vim.api.nvim_create_autocmd("ColorScheme", {
+                pattern = "*",
+                callback = set_normal_float_highlight,
+            })
+
+            require("gruvbox").setup({
+                transparent_mode = true,
+            })
+            vim.o.background = "dark" -- or "light" for light mode
+            vim.cmd([[colorscheme gruvbox]])
         end
-
-        vim.api.nvim_create_autocmd("ColorScheme", {
-            pattern = "*",
-            callback = set_normal_float_highlight,
-        })
-
-        require("gruvbox").setup({
-            transparent_mode = true,
-        })
-        vim.o.background = "dark" -- or "light" for light mode
-        vim.cmd([[colorscheme gruvbox]])
-    end
+    },
+    { 'nvim-mini/mini.statusline', version = '*', config = true },
 }
