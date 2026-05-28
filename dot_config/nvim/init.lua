75985bcef170d756c5fa85fd52d2489333ff0483
Simon Théorêt <stheoret@druide.com>
2026-05-28 15:40:53 -0400

    feat(nvim): remove whichkey and luarocks

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -13,6 +13,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         os.exit(1)
     end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 vim.o.termguicolors = true
@@ -93,6 +94,7 @@ vim.opt.completeopt = "menu,menuone,popup,fuzzy,noselect"
 
 
 require("lazy").setup({
+    rocks = {enabled = false},
     pkg = {
         enabled = true,
         cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
