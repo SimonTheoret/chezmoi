116bd7189c97151103ed85b16ca41d89ad382479
Simon Théorêt <stheoret@druide.com>
2026-07-07 16:59:12 -0400

    feat: added qcode.sh

---
+++ bin/executable_qcode.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env sh
+
+printf "
+Problème:
+
+But:
+
+Entrées:
+
+Sorties attendues:
+
+Contraintes:
+
+Structures de données pertinentes:
+
+Sous-problèmes:
+
+Cas limites:
+
+Risques d'erreurs:
+
+Stratégie de tests:
+
+"
