86cf593513f5b727c5e32b479d58a44d20412100
Simon Théorêt <stheoret@druide.com>
2026-08-04 10:18:17 -0400

    Revert "fix: command runner for helix"
    This reverts commit 21007f7cb708bb330b22d530ae305318d9fbba39.

--- bin/executable_zr.fish
+++ bin/executable_zr.fish
@@ -8,17 +8,17 @@ Compilation: '"
 echo "=========" >>$LOGFILE
 echo "FILE: $FILE"
 
-# if test -n $__HELIX_LAST_CMD_FILE
-#     set default_cmd "$(cat $__HELIX_LAST_CMD_FILE)"
-#     if test $(string length $default_cmd) -gt 0
-#         set read_cmd "$read_cmd -c \"$default_cmd\""
-echo "The read command is '$read_cmd'" >>$LOGFILE
-#     else
-#         echo "The command inside `__HELIX_LAST_CMD_FILE` is empty: '$default_cmd' " >>$LOGFILE
-#     end
-# else
-#     echo "Variable `__HELIX_LAST_CMD_FILE` is empty or does not exist." >>$LOGFILE
-# end
+if test -n $__HELIX_LAST_CMD_FILE
+    set default_cmd "$(cat $__HELIX_LAST_CMD_FILE)"
+    if test $(string length $default_cmd) -gt 0
+        set read_cmd "$read_cmd -c \"$default_cmd\""
+        echo "The read command is '$read_cmd'" >>$LOGFILE
+    else
+        echo "The command inside `__HELIX_LAST_CMD_FILE` is empty: '$default_cmd' " >>$LOGFILE
+    end
+else
+    echo "Variable `__HELIX_LAST_CMD_FILE` is empty or does not exist." >>$LOGFILE
+end
 
 set PANE_1_Id $(zellij run -f --block-until-exit -c -n compilation -- fish -c "echo ($read_cmd) >>\"$FILE\"")
 
