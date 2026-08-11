3568545e5c755837aa0ef287fcfd667a0afc6664
Simon Théorêt <stheoret@druide.com>
2026-08-11 18:32:01 -0400

    feat(nvim): make nvim.sh multiplatform (kindof)

--- bin/build/executable_nvim.sh
+++ bin/build/executable_nvim.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
 
-rm -r ~/bin/nvim-linux-x86_64*
-rm -r ~/bin/build/nvim-linux-x86_64*
-wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-tar -xzf nvim-linux-x86_64.tar.gz
-[ -d "~/bin/nvim-linux-x86_64" ] && rm -r ~/bin/nvim-linux-x86_64*
-mv ~/bin/build/nvim-linux-x86_64 ~/bin/
+ARCH=$(uname -m)
+OS=$(uname)
+if [ "$OS" = "Darwin" ]; then
+	OS="macos"
+else
+	OS="linux"
+fi
+VERSION="nvim-${OS}-${ARCH}"
+rm -r "${HOME:?}"/bin/nvim*
+rm -r "${HOME:?}"/bin/build/nvim-*
+wget "https://github.com/neovim/neovim/releases/latest/download/$VERSION.tar.gz"
+tar -xzf "$VERSION.tar.gz"
+[ -d "${HOME:?}/bin/$VERSION" ] && rm -r "${HOME:?}/bin/$VERSION*"
+mv "${HOME:?}/bin/build/$VERSION" ~/bin/
