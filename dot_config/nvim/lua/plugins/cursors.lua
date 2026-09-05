2f989bcc8cd0847e513e75cf4c2acbae86f81d3b
Simon Théorêt <simonteoret@hotmail.com>
2026-09-04 22:56:43 -0400

    feat(nvim): added fff

--- dot_config/nvim/lua/plugins/cursors.lua
+++ dot_config/nvim/lua/plugins/cursors.lua
@@ -1,7 +1,7 @@
 return {
     "jake-stewart/multicursor.nvim",
     branch = "1.0",
-    event = "VeryLazy",
+    event = "InsertEnter",
     config = function()
         local mc = require("multicursor-nvim")
         mc.setup()
