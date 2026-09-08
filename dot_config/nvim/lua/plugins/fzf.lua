88e09b9e6b45eda50e503d1b1b0cb9175fa11d54
Simon Théorêt <stheoret@druide.com>
2026-09-08 09:42:00 -0400

    feat(nvim): remove fzf-lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -34,112 +34,113 @@ return {
                 },
             },
         }
-    },
-    {
-        "ibhagwan/fzf-lua",
-        -- optional for icon support
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            local fzf = require("fzf-lua")
-            -- calling `setup` is optional for customization
-            fzf.setup({
-                { "max-perf" },
-                grep = { rg_glob = true },
-                keymap = {
-                    fzf = {
-                        ["ctrl-a"] = "select-all+accept",
-                    }
-                },
-            })
-        end,
-        keys = {
-            {
-                "<leader>fA",
-                function()
-                    require('fzf-lua').files({ no_ignore = true })
-                end,
-                desc = "List all files"
-            },
-
-            {
-                "<C-x><C-f>",
-                function() require("fzf-lua").complete_path() end,
-                silent = true,
-                desc = "Fuzzy complete path",
-                mode = { "n", "v", "i" }
-            },
-            {
-                "<leader>fb",
-                function()
-                    require('fzf-lua').buffers()
-                end,
-                desc = "List buffers"
-            },
-            {
-                "<leader>fk",
-                function()
-                    require('fzf-lua').keymaps({ previewer = false })
-                end,
-                desc = "List keymaps"
-            },
-            {
-                "<leader>fH",
-                function()
-                    require('fzf-lua').helptags()
-                end,
-                desc = "Vim help"
-            },
-            {
-                "<leader>fh",
-                function()
-                    require('fzf-lua').manpages()
-                end,
-                desc = "Man pages"
-            },
-            {
-                '<leader>fI',
-                function() require('fzf-lua').lsp_finder() end,
-                desc = "Lsp finder at cursor"
-            },
-            {
-                '<leader>fJ',
-                function() vim.lsp.buf.workspace_symbol() end,
-                desc = "Lsp workspace symbols query"
-            },
-            {
-                '<leader>fj',
-                function() require('fzf-lua').lsp_document_symbols() end,
-                desc = "Lsp document symbols"
-            },
-            { "gai", function() require('fzf-lua').lsp_incoming_calls() end, desc = "C[a]lls Incoming" },
-            { "gao", function() require('fzf-lua').lsp_outgoing_calls() end, desc = "C[a]lls Outgoing" },
-            {
-                '<leader>fi',
-                function() require('fzf-lua').lsp_live_workspace_symbols() end,
-                desc = "lsp live workspace symbols"
-            },
-            {
-                '<leader>fr',
-                function() require('fzf-lua').lsp_references() end,
-                desc = "lsp references"
-            },
-            {
-                '<leader>fd',
-                function() require('fzf-lua').diagnostics_document() end,
-                desc = "Document diagnostics"
-            },
-            {
-                '<leader>fD',
-                function() require('fzf-lua').diagnostics_workspace() end,
-                desc = "Workspace diagnostics"
-            },
-            {
-                "<leader>'",
-                function()
-                    require('fzf-lua').resume()
-                end,
-                desc = "Fzf resume"
-            }
-        }
     }
+    -- ,
+    -- {
+        -- "ibhagwan/fzf-lua",
+        -- -- optional for icon support
+        -- dependencies = { "nvim-tree/nvim-web-devicons" },
+        -- config = function()
+        --     local fzf = require("fzf-lua")
+        --     -- calling `setup` is optional for customization
+        --     fzf.setup({
+        --         { "max-perf" },
+        --         grep = { rg_glob = true },
+        --         keymap = {
+        --             fzf = {
+        --                 ["ctrl-a"] = "select-all+accept",
+        --             }
+        --         },
+        --     })
+        -- end,
+        -- keys = {
+        --     {
+        --         "<leader>fA",
+        --         function()
+        --             require('fzf-lua').files({ no_ignore = true })
+        --         end,
+        --         desc = "List all files"
+        --     },
+        --
+        --     {
+        --         "<C-x><C-f>",
+        --         function() require("fzf-lua").complete_path() end,
+        --         silent = true,
+        --         desc = "Fuzzy complete path",
+        --         mode = { "n", "v", "i" }
+        --     },
+        --     {
+        --         "<leader>fb",
+        --         function()
+        --             require('fzf-lua').buffers()
+        --         end,
+        --         desc = "List buffers"
+        --     },
+        --     {
+        --         "<leader>fk",
+        --         function()
+        --             require('fzf-lua').keymaps({ previewer = false })
+        --         end,
+        --         desc = "List keymaps"
+        --     },
+        --     {
+        --         "<leader>fH",
+        --         function()
+        --             require('fzf-lua').helptags()
+        --         end,
+        --         desc = "Vim help"
+        --     },
+        --     {
+        --         "<leader>fh",
+        --         function()
+        --             require('fzf-lua').manpages()
+        --         end,
+        --         desc = "Man pages"
+        --     },
+        --     {
+        --         '<leader>fI',
+        --         function() require('fzf-lua').lsp_finder() end,
+        --         desc = "Lsp finder at cursor"
+        --     },
+        --     {
+        --         '<leader>fJ',
+        --         function() vim.lsp.buf.workspace_symbol() end,
+        --         desc = "Lsp workspace symbols query"
+        --     },
+        --     {
+        --         '<leader>fj',
+        --         function() require('fzf-lua').lsp_document_symbols() end,
+        --         desc = "Lsp document symbols"
+        --     },
+        --     { "gai", function() require('fzf-lua').lsp_incoming_calls() end, desc = "C[a]lls Incoming" },
+        --     { "gao", function() require('fzf-lua').lsp_outgoing_calls() end, desc = "C[a]lls Outgoing" },
+        --     {
+        --         '<leader>fi',
+        --         function() require('fzf-lua').lsp_live_workspace_symbols() end,
+        --         desc = "lsp live workspace symbols"
+        --     },
+        --     {
+        --         '<leader>fr',
+        --         function() require('fzf-lua').lsp_references() end,
+        --         desc = "lsp references"
+        --     },
+        --     {
+        --         '<leader>fd',
+        --         function() require('fzf-lua').diagnostics_document() end,
+        --         desc = "Document diagnostics"
+        --     },
+        --     {
+        --         '<leader>fD',
+        --         function() require('fzf-lua').diagnostics_workspace() end,
+        --         desc = "Workspace diagnostics"
+        --     },
+        --     {
+        --         "<leader>'",
+        --         function()
+        --             require('fzf-lua').resume()
+        --         end,
+        --         desc = "Fzf resume"
+        --     }
+        -- }
+    -- }
 }
